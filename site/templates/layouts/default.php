d4181c7261339401aa395db312fdf1eb121d3e78
edwin <edwin@questic.nl>
2022-09-19 15:40:02 +0200

    aanpassing layout default

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -2,13 +2,13 @@
 <html lang="<?= $lang ?>">
     <head>
 
-    <meta charset="utf-8">
-        <title><?= $page->title ?> | <?= $this->site_name ?></title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://use.typekit.net/nnq4ois.css">
-        <link rel="stylesheet" href="<?= $config->urls->templates ?>styles/main.css">
-        <script src="https://unpkg.com/vue@3"></script>
+<meta charset="utf-8">
+<title><?= $page->title ?> | <?= $this->site_name ?></title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://use.typekit.net/nnq4ois.css">
+<link rel="stylesheet" href="<?= $config->urls->templates ?>styles/main.css">
+	<script src="https://unpkg.com/vue@3"></script>
     </head>
     <body>
         <div class="main-container">
