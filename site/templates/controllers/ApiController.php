92091af7464bf2ea6a6e38d8f18781046db40481
EdwinStolze <edwin@questic.nl>
2022-10-13 15:49:48 +0200

    wip: investigate options

--- site/templates/controllers/ApiController.php
+++ site/templates/controllers/ApiController.php
@@ -11,12 +11,27 @@ class APIController extends \Wireframe\Controller {
      * Render method
      */
     public function render() {
-        // echo $this->wire('modules')->get('WireframeAPI')->init()->sendHeaders()->render();
         $api = $this->wire('modules')->get('WireframeAPI') ;
+        $api->addEndpoint('basic-page', function($path, $args) {
+            // var_dump($path);
+            $this->page = $this->pages->get("template=basic-page,name=".$path[0]);
+            return [
+                'path' => $path,
+                'args' => $args,
+                // 'url' => $this->pages->get(1042)->url,
+                'url' => $this->page->url,
+                'json' => [
+                    "title" => $this->page->title,
+                    "body" => $this->page->body,
+                    "firstChild" => $this->page->children->first->id,
+                    "nextPage" => $this->page->next()->id
+                ]
+            ];
+        });
         echo $api->init()->sendHeaders()->render();
         $this->view->setLayout(null)->halt();
     }
-
+    
     public function renderJSON(): ?string {
         return json_encode([
             "title" => $this->page->title,
