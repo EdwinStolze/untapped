d146d435ae8a16ef71a10d184edac8e5550c7211
Edwin Stolze <edwin@questic.nl>
2023-03-14 15:07:05 +0100

    simply driverscontroller

--- site/templates/controllers/DriversController.php
+++ site/templates/controllers/DriversController.php
@@ -14,75 +14,45 @@ class DriversController extends \Wireframe\Controller
         // $this->view->setLayout(null)->halt();
     }
 
-    private function createDriverArray($driver)
-    {
-
-        $quotes = [];
-        foreach ($driver->quotes as $quote) {
-            array_push($quotes, array(
-                'quote' => $quote->quote,
-                'author' => $quote->author
-            ));
-        }
-
-        return array(
-            'id' => $driver->id,
-            'title' => $driver->title,
-            'title_long' => $driver->title_long,
-            'icon_name' => $driver->icon_name,
-            'explanation' => $driver->explanation,
-            'quotes' => $quotes,
-        );
-    }
-
-    public function &getItemFromArray($key, $value, &$array)
-    {
-        foreach ($array as &$item) {
-            if ($item[$key] == $value) return $item;
-        }
-        return FALSE;
-    }
-
     public function renderJSON(): ?string
     {
         $driverslist = array(
-            "version" => 1,
-            "types" => []
+            "version"           => 1,
+            'next_page'         => $this->page->next_page->id,
+            'vue_router_name'   => $this->page->vue_router_name,
+            'button_type'       => $this->page->button_type,
+            'button_name'       => $this->page->button_name,
+            'body'              => $this->page->body,
+            'drivers'           => [],
+            'table'             => [],
         );
 
         foreach ($this->pages->find("template=driver, sort=sort") as $driver) {
 
-            // Create drivers group for driver_type
-            if (!$this->getItemFromArray('id', $driver->driver_type->id, $driverslist['types'])) {
-
-                $driver_groups = [];
-                foreach ($this->pages->find("template=driver_group") as $driver_group) {
-                    array_push($driver_groups, array(
-                        "id" => $driver_group->id,
-                        "name" => $driver_group->name,
-                        "drivers" => []
-                    ));
-                }
-
-                // Create drive_type
-                array_push(
-                    $driverslist['types'],
-                    array(
-                        "id" => $driver->driver_type->id,
-                        "name" => $driver->driver_type->name,
-                        "groups" => $driver_groups
-                    )
-                );
-                
+            $chapters = [];
+            foreach ($driver->children() as $chapter) {
+                array_push($chapters, array(
+                    'id'            => $chapter->id,
+                    'template'      => $chapter->template->name,
+                    'title'         => $chapter->title,
+                    'body'          => $chapter->body,
+                ));
             }
 
-            // Add driver to the appropriate driver group
-            $type   = &$this->getItemFromArray('id', $driver->driver_type->id, $driverslist['types']);
-            $group  = &$this->getItemFromArray('id', $driver->parent->id, $type ['groups']);
-            $group['drivers'][] = $this->createDriverArray($driver);
-
+            array_push($driverslist['drivers'], array(
+                'id'                => $driver->id,
+                'name'              => $driver->name,
+                'title'             => $driver->title,
+                'driver_group'      => $driver->parent->name,
+                'driver_group_id'   => $driver->parent->id,
+                'driver_type'       => $driver->driver_type->name,
+                'long_title'        => $driver->title_long,
+                'icon_name'         => $driver->icon_name,
+                'explanation'       => $driver->explanation,
+                'chapters'          => $chapters,
+                'quotes'            => [],
+            ));
         }
         return json_encode($driverslist, true);
     }
-
 }
